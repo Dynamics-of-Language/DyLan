7f2f15f19a21b39beaaa10a6da66f6db068c7168
Julian Hough <julianchough@gmail.com>
2015-04-22 17:57:49 +0200

    Getting rid of unused imports

--- src/java/qmul/ds/ttrlattice/TTRLattice.java
+++ src/java/qmul/ds/ttrlattice/TTRLattice.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import edu.stanford.nlp.util.ArrayUtils;
 import ptolemy.graph.DirectedAcyclicGraph;
 import ptolemy.graph.Node;
 import qmul.ds.formula.TTRRecordType;
