242b9cb1247bb3a164f4a58c28c69e6d4aaad4a2
Matthew Purver <matt@chatterbox.co>
2015-08-14 11:45:33 +0100

    applied standard formatting

--- src/java/qmul/ds/learn/TestParser.java
+++ src/java/qmul/ds/learn/TestParser.java
@@ -26,7 +26,7 @@ public class TestParser extends ContextParser {
 
 	RecordTypeCorpus testCorpus;
 	List<Word> unknownWordsEncountered;
-	
+
 	public static void pause() {
 		System.out.println("Press enter to continue...");
 		try {
@@ -40,14 +40,15 @@ public class TestParser extends ContextParser {
 		super(resourceDir);
 		unknownWordsEncountered = new ArrayList<Word>();
 	}
-	
-	public TestParser(String resourceDir){
+
+	public TestParser(String resourceDir) {
 		super(resourceDir);
 		unknownWordsEncountered = new ArrayList<Word>();
 	}
 
 	/**
 	 * Gets parser from:
+	 * 
 	 * @param resourcePath
 	 * @return
 	 */
@@ -55,11 +56,11 @@ public class TestParser extends ContextParser {
 		Lexicon lex = null;
 		try {
 			ObjectInputStream in = new ObjectInputStream(new FileInputStream(resourcePath));
-			//ObjectInputStream in = new ObjectInputStream(new FileInputStream(resourceDir + "lexicon.3.lex"));
+			// ObjectInputStream in = new ObjectInputStream(new FileInputStream(resourceDir + "lexicon.3.lex"));
 			lex = (Lexicon) in.readObject();
 			in.close();
-			//ContextParser p = new ContextParser(resourceDir);
-			//lex = p.getLexicon();
+			// ContextParser p = new ContextParser(resourceDir);
+			// lex = p.getLexicon();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -77,13 +78,15 @@ public class TestParser extends ContextParser {
 	}
 
 	/**
-	 * @param corpusFile file of utterances to read in
-	 * @param hasTargetFormulae whether the file has TTR record types or not
+	 * @param corpusFile
+	 *            file of utterances to read in
+	 * @param hasTargetFormulae
+	 *            whether the file has TTR record types or not
 	 */
 	public void loadTestCorpus(String corpusFile, boolean hasTargetFormulae) {
 		try {
 			this.testCorpus = new RecordTypeCorpus();
-			if (hasTargetFormulae){
+			if (hasTargetFormulae) {
 				this.testCorpus.loadCorpus(new File(corpusFile));
 			} else {
 				this.testCorpus.loadCorpusNoRecordTypes(new File(corpusFile));
@@ -97,53 +100,53 @@ public class TestParser extends ContextParser {
 		boolean unknown = false;
 		for (Word w : s)
 			if (!lexicon.containsKey(w.word()))
-				if (!unknownWordsEncountered.contains(w)){
+				if (!unknownWordsEncountered.contains(w)) {
 					unknownWordsEncountered.add(w);
 				}
-				unknown = true;
+		unknown = true;
 		return unknown;
 	}
 
 	/**
-	 * Method to write the incremental ttr output from parsing corpus, i.e. the word-by-word maximal semantics, to a file.
-	 * Also outputs (additive) difference between output at previous word and that at the current one, including relevant dependent fields. e.g.:
+	 * Method to write the incremental ttr output from parsing corpus, i.e. the word-by-word maximal semantics, to a
+	 * file. Also outputs (additive) difference between output at previous word and that at the current one, including
+	 * relevant dependent fields. e.g.:
 	 * 
-	 * uttnumber : (UTTID)
-	 * [word]  		[currentTTR]    				         [diff]
-	 * take         [x|take(x)]         			         [x|take(x)]
-	 * the          [x|def(x)|take(x)]         				 [x|def(x)]
-	 * red          [x|red(x)|def(x)|take(x)]  				 [x|red(x)]
-	 * cross        [x|cross(x)|red(x)|def(x)|take(x)]       [x|cross(x)]
+	 * uttnumber : (UTTID) [word] [currentTTR] [diff] take [x|take(x)] [x|take(x)] the [x|def(x)|take(x)] [x|def(x)] red
+	 * [x|red(x)|def(x)|take(x)] [x|red(x)] cross [x|cross(x)|red(x)|def(x)|take(x)] [x|cross(x)]
 	 * 
-	 * @param outputFile the file to write the corpus to
-	 * @param errorFile the file to write all the parsing errors and summary at the end
-	 * @param beam the number of parses to maintain in the parse state after each word, puts ceiling on decoding time
-	 * @param hasUtteranceIndices boolean as to whether this file has an utterance index at the end of each line or not
-	**/ 
-	public void parseCorpusToFile(String outputFile, String errorFile, int beam, boolean hasUtteranceIndices){
-		 
-		//set the file up
+	 * @param outputFile
+	 *            the file to write the corpus to
+	 * @param errorFile
+	 *            the file to write all the parsing errors and summary at the end
+	 * @param beam
+	 *            the number of parses to maintain in the parse state after each word, puts ceiling on decoding time
+	 * @param hasUtteranceIndices
+	 *            boolean as to whether this file has an utterance index at the end of each line or not
+	 **/
+	public void parseCorpusToFile(String outputFile, String errorFile, int beam, boolean hasUtteranceIndices) {
+
+		// set the file up
 		FileWriter errorFileStream = null;
 		FileWriter outputFileStream = null;
 		BufferedWriter out;
-			
-		try{
+
+		try {
 			errorFileStream = new FileWriter(errorFile);
 			outputFileStream = new FileWriter(outputFile);
-			out = new BufferedWriter(errorFileStream);			
-		} catch(IOException e){ 
+			out = new BufferedWriter(errorFileStream);
+		} catch (IOException e) {
 			System.out.println("Could not create text files");
 		}
-		
-		
-		int notParsed = 0; //number of successful parses without crashing
+
+		int notParsed = 0; // number of successful parses without crashing
 		Iterator<Pair<Sentence<Word>, TTRRecordType>> corpusIt = testCorpus.iterator();
 		int count = 0;
-		corpusLoop : while (corpusIt.hasNext()) {
+		corpusLoop: while (corpusIt.hasNext()) {
 			Pair<Sentence<Word>, TTRRecordType> entry = corpusIt.next();
 			String ID = testCorpus.getIndexNumber(count);
-			count+=1;
-			try { 
+			count += 1;
+			try {
 				containsUnknown(entry.first);
 				init();
 				String successfulPrefix = "";
@@ -154,14 +157,14 @@ public class TestParser extends ContextParser {
 				boolean parsed = true;
 				int stateMarker = 1;
 				for (HasWord word : entry.first) {
-					//System.out.println(word.word());
-					//System.out.println(stateHistory);
+					// System.out.println(word.word());
+					// System.out.println(stateHistory);
 					parseWord(word.word());
-					if (!successful()){
-						//need to store after which word it broke down
-						parsed=false;
+					if (!successful()) {
+						// need to store after which word it broke down
+						parsed = false;
 						try {
-							out = new BufferedWriter(errorFileStream);	
+							out = new BufferedWriter(errorFileStream);
 							out.write(ID);
 							out.newLine();
 							out.write(successfulPrefix);
@@ -169,90 +172,93 @@ public class TestParser extends ContextParser {
 							out.write(entry.first.toString());
 							out.newLine();
 							out.flush();
-						} catch(IOException e) {
+						} catch (IOException e) {
 							System.out.println("Could not write to error file");
 						}
-						//simple repair backtrack
-						while (stateMarker>=0){
-							stateMarker=stateMarker-1;
+						// simple repair backtrack
+						while (stateMarker >= 0) {
+							stateMarker = stateMarker - 1;
 							setState(stateHistory.get(stateMarker));
 							parseWord(word.word());
-							if (successful()){
-								//setSuccessful(true);
+							if (successful()) {
+								// setSuccessful(true);
 								break;
 							}
 						}
 					}
-					
-					//check to see if it hasn't managed a parse at all, despite backtracking. Shouldn't it output the previous one?
-					if (!successful()){
+
+					// check to see if it hasn't managed a parse at all, despite backtracking. Shouldn't it output the
+					// previous one?
+					if (!successful()) {
 						wordTTRoutput[prefixPosition] = TTRRecordType.parse("[]");
-						prefixPosition+=1;
+						prefixPosition += 1;
 						continue;
 					}
-					//get the top n states (beam)
-					ParseState<ContextParserTuple> nBest = getStateWithNBestTuples(beam); //retains the top beamwidth
+					// get the top n states (beam)
+					ParseState<ContextParserTuple> nBest = getStateWithNBestTuples(beam); // retains the top beamwidth
 					setState(nBest);
 					stateHistory.add(nBest);
-					stateMarker+=1;
-					ParseState<ContextParserTuple> Best = getStateWithNBestTuples(1); //just outputs the current best TTR
+					stateMarker += 1;
+					ParseState<ContextParserTuple> Best = getStateWithNBestTuples(1); // just outputs current best TTR
 					Iterator<ContextParserTuple> iter = Best.iterator();
 					while (iter.hasNext()) {
 						TTRRecordType ttr = (TTRRecordType) iter.next().getTree().getMaximalSemantics();
-						if (ttr==null){ttr = TTRRecordType.parse("[]");}
+						if (ttr == null) {
+							ttr = TTRRecordType.parse("[]");
+						}
 						wordTTRoutput[prefixPosition] = ttr;
 					}
-					successfulPrefix+=word.toString()+" ";
-					prefixPosition+=1;		
+					successfulPrefix += word.toString() + " ";
+					prefixPosition += 1;
 				}
-				//check success of parse and write the word-by-word TTR outputs to the file
-			    if (parsed==false){
-			    	notParsed++;
-			    }
+				// check success of parse and write the word-by-word TTR outputs to the file
+				if (parsed == false) {
+					notParsed++;
+				}
 				TTRRecordType currentTTRrec = TTRRecordType.parse("[]");
-				for (int r=0; r<wordTTRoutput.length; r++) {
+				for (int r = 0; r < wordTTRoutput.length; r++) {
 					try {
-						out = new BufferedWriter(outputFileStream);	
+						out = new BufferedWriter(outputFileStream);
 						out.write(ID + "\t");
-						out.write(entry.first.get(r).toString()+"\t");
-						out.write(wordTTRoutput[r].toString()+"\t");
-						//get the diff
+						out.write(entry.first.get(r).toString() + "\t");
+						out.write(wordTTRoutput[r].toString() + "\t");
+						// get the diff
 						out.write(wordTTRoutput[r].minus(currentTTRrec).first.toString());
 						currentTTRrec = wordTTRoutput[r];
 						out.newLine();
 						out.flush();
-					} catch(IOException e) {
+					} catch (IOException e) {
 						System.out.println("Could not write to ouput file");
 					}
 				}
-					
+
 			} catch (Exception f) {
 				System.out.println("could not parse utterance!");
 				f.printStackTrace();
 			}
 		}
-		
-		//print parse errors summary to error File
+
+		// print parse errors summary to error File
 		try {
-			out = new BufferedWriter(errorFileStream);	
+			out = new BufferedWriter(errorFileStream);
 			out.write("Number of unknown words = " + String.valueOf(this.unknownWordsEncountered.size()));
 			out.newLine();
-			for (Word word : this.unknownWordsEncountered){
+			for (Word word : this.unknownWordsEncountered) {
 				out.write(word.toString());
 				out.newLine();
 			}
 			out.newLine();
 			out.write("Number of bad parses = " + String.valueOf(notParsed));
 			out.newLine();
-			out.write("Proportion of bad parses = " + String.valueOf((float) notParsed/count));
+			out.write("Proportion of bad parses = " + String.valueOf((float) notParsed / count));
 			out.flush();
 			out.close();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println("Could not write to errorfile");
 		}
-		
+
 	}
-	
+
 	/**
 	 * Tests the test corpus and outputs evaluation metrics
 	 */
@@ -262,40 +268,39 @@ public class TestParser extends ContextParser {
 		int total = 0;
 		List<TTRRecordType[]> myttrs = new ArrayList<TTRRecordType[]>();
 		Evaluation e = new Evaluation();
-		
+
 		if (testCorpus == null)
 			return;
 		Iterator<Pair<Sentence<Word>, TTRRecordType>> corpusIt = testCorpus.iterator();
-		corpusLoop : while (corpusIt.hasNext()) {
-			
+		corpusLoop: while (corpusIt.hasNext()) {
+
 			Pair<Sentence<Word>, TTRRecordType> entry = corpusIt.next();
 			/*
-			//skipping utterances with unknown words at test time or not?
-			if (this.containsUnknown(entry.first())){
-				System.out.println(entry.first());
-				pause();pause();
-				continue;
-			}*/
+			 * //skipping utterances with unknown words at test time or not? if (this.containsUnknown(entry.first())){
+			 * System.out.println(entry.first()); pause();pause(); continue; }
+			 */
 			total++;
 			init();
 
 			TTRRecordType[] pair = new TTRRecordType[2];
 			TTRRecordType targetRT = entry.second();
 			pair[1] = targetRT;
-			
+
 			System.out.println("Parsing " + entry.first);
-			
+
 			float mostNodesMapped = 0;
-			try { 
+			try {
 				if (parse(entry.first)) {
 					System.out.println("parsed: " + entry.first);
 					parsed++;
 					ParseState<ContextParserTuple> twoBest = getStateWithNBestTuples(10);
 					Iterator<ContextParserTuple> iter = twoBest.iterator();
-	
+
 					while (iter.hasNext()) {
 						TTRRecordType ttr = (TTRRecordType) iter.next().getTree().getMaximalSemantics();
-						if (ttr==null){ttr = TTRRecordType.parse("[]");}
+						if (ttr == null) {
+							ttr = TTRRecordType.parse("[]");
+						}
 						if (ttr.subsumes(targetRT) && targetRT.subsumes(ttr)) {
 							System.out.println("same formula/maximal match");
 							pair[0] = ttr;
@@ -304,9 +309,9 @@ public class TestParser extends ContextParser {
 							continue corpusLoop;
 						} else {
 							float mapped = e.totalNodesMapped(ttr, targetRT);
-							if (mapped>=mostNodesMapped){
-								mostNodesMapped = mapped; 
-								pair[0] = ttr;//always gets the highest mapped of the top two parses
+							if (mapped >= mostNodesMapped) {
+								mostNodesMapped = mapped;
+								pair[0] = ttr;// always gets the highest mapped of the top two parses
 							}
 						}
 					}
@@ -317,10 +322,10 @@ public class TestParser extends ContextParser {
 			} catch (Exception f) {
 				pair[0] = TTRRecordType.parse("[]");
 			}
-			
+
 			myttrs.add(pair);
 		}
-				
+
 		List<Float> macro = e.precisionRecallMacro(myttrs);
 		EvaluationResult micro = e.precisionRecallMicro(myttrs);
 		System.out.println(parsed);
@@ -339,9 +344,10 @@ public class TestParser extends ContextParser {
 
 	public static void main(String a[]) {
 		TestParser tp = new TestParser("resource/2014-german-ttr-pento");
-		//pause();
-		tp.loadTestCorpus("corpus\\Pento\\PENTO_gold-1.transcript.txt",false);
-		tp.parseCorpusToFile("C:\\users\\julian\\TestTTRPentoCorpusErrors.txt","C:\\users\\julian\\TestTTRPentoCorpusRTs.txt",30,true);
+		// pause();
+		tp.loadTestCorpus("corpus\\Pento\\PENTO_gold-1.transcript.txt", false);
+		tp.parseCorpusToFile("C:\\users\\julian\\TestTTRPentoCorpusErrors.txt",
+				"C:\\users\\julian\\TestTTRPentoCorpusRTs.txt", 30, true);
 	}
 
 }
