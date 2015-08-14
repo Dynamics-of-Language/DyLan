ef32aa41646b5f06f79844a0c59f7257b8148a48
Matthew Purver <matt@chatterbox.co>
2015-08-14 11:49:42 +0100

    applied standard formatting, commented out unfinished bits

--- src/java/qmul/ds/ttrlattice/TTRLattice.java
+++ src/java/qmul/ds/ttrlattice/TTRLattice.java
@@ -15,8 +15,6 @@ import qmul.ds.formula.Variable;
 
 public class TTRLattice extends DirectedAcyclicGraph {
 
-	
-
 	public static void pause() {
 		System.out.println("Press enter to continue...");
 		try {
@@ -25,83 +23,83 @@ public class TTRLattice extends DirectedAcyclicGraph {
 		}
 
 	}
+
 	public TTRLattice() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public Node nodeFromTTRRecordType(TTRRecordType ttr){
-		for (Object i : this.nodes()){
-			//System.out.println(((TTRLatticeNode)i).ttr);
-			if (((TTRLatticeNode)((Node)i).getWeight()).ttr==null){
+
+	public Node nodeFromTTRRecordType(TTRRecordType ttr) {
+		for (Object i : this.nodes()) {
+			// System.out.println(((TTRLatticeNode)i).ttr);
+			if (((TTRLatticeNode) ((Node) i).getWeight()).ttr == null) {
 				continue;
 			}
-			
-			if (((TTRLatticeNode)((Node)i).getWeight()).ttr.equals(ttr)){
-				return (Node)i;
+
+			if (((TTRLatticeNode) ((Node) i).getWeight()).ttr.equals(ttr)) {
+				return (Node) i;
 			}
 		}
 		return null;
 	}
-	
-	public boolean containsTTRRecordType(TTRRecordType ttr){
-		for (Object i : this.nodes()){
-			//System.out.println(((TTRLatticeNode)i).ttr);
-			if (((TTRLatticeNode)((Node)i).getWeight()).ttr==null){
+
+	public boolean containsTTRRecordType(TTRRecordType ttr) {
+		for (Object i : this.nodes()) {
+			// System.out.println(((TTRLatticeNode)i).ttr);
+			if (((TTRLatticeNode) ((Node) i).getWeight()).ttr == null) {
 				continue;
 			}
-			
-			if (((TTRLatticeNode)((Node)i).getWeight()).ttr.equals(ttr)){
+
+			if (((TTRLatticeNode) ((Node) i).getWeight()).ttr.equals(ttr)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	/***
 	 * The main ordering relation computation for two record type nodes that are already in the lattice
+	 * 
 	 * @param node1
 	 * @param node2
 	 */
-	public void order(Node node1, Node node2){
-		//TODO need the most efficient way of ordering two nodes
-		//make it recursive..
-		//case 1- it's a proper supertype (not the same) as current node- order it in the subtype relation
-		//find the supertypes it is a subtype of (if any) and order
-		TTRLatticeNode ttrLat = (TTRLatticeNode) agenda.get(0);
-		if (node2.getWeight()) {
-			
-		}
-		//case 2- it's not a type of current node- just return (should stop the recursion)
-		//case 3- it's a proper subtype of 
+	public void order(Node node1, Node node2) {
+		// TODO need the most efficient way of ordering two nodes
+		// make it recursive..
+		// case 1- it's a proper supertype (not the same) as current node- order it in the subtype relation
+		// find the supertypes it is a subtype of (if any) and order
+		// TTRLatticeNode ttrLat = (TTRLatticeNode) agenda.get(0);
+		// if (node2.getWeight()) {
+		//
+		// }
+		// case 2- it's not a type of current node- just return (should stop the recursion)
+		// case 3- it's a proper subtype of
 		return;
 	}
-	
+
 	/***
 	 * The ordering operation for a new node which hasn't been ordered yet to other nodes in the lattice
+	 * 
 	 * @param node1
-	 * @throws GraphActionException 
+	 * @throws GraphActionException
 	 */
-	public void orderNode(Node node1) throws GraphActionException{
-		//TODO should be top down search
-		//Go from top then downward path retuning upon hitting bottom
-		//recurse and also keep list of checked nodes, only need to order this one node with each node once
-		//List<Object> obs = this.topologicalSort(this.nodes());
-		
-		order((Node)this.bottom(), (Node) node1); //recurse from bottom??
-		
-		//for (Object node : obs){
-			//TODO
-		//	order((Node) node, node1);
-			
-		//}
-		
-		
+	public void orderNode(Node node1) throws GraphActionException {
+		// TODO should be top down search
+		// Go from top then downward path retuning upon hitting bottom
+		// recurse and also keep list of checked nodes, only need to order this one node with each node once
+		// List<Object> obs = this.topologicalSort(this.nodes());
+
+		order((Node) this.bottom(), (Node) node1); // recurse from bottom??
+
+		// for (Object node : obs){
+		// TODO
+		// order((Node) node, node1);
+
+		// }
+
 	}
 
-
-	
-	public void constructFromAtoms(List<TTRRecordType> myttr, List<Set<TTRAustinianProp>> myprops){
+	public void constructFromAtoms(List<TTRRecordType> myttr, List<Set<TTRAustinianProp>> myprops) {
 		/*
 		 * Constructs the TTR lattice from the bottom up as in Hough and Purver 2014 TTNLS
 		 */
@@ -109,93 +107,98 @@ public class TTRLattice extends DirectedAcyclicGraph {
 		abottom.setBottom();
 		Node bottom = new Node(abottom);
 		this.addNode(bottom);
-	
-		//atoms
-		for (int i=0; i<myttr.size(); i++){
-			Node n = new Node(new TTRLatticeNode(myttr.get(i),myprops.get(i)));
+
+		// atoms
+		for (int i = 0; i < myttr.size(); i++) {
+			Node n = new Node(new TTRLatticeNode(myttr.get(i), myprops.get(i)));
 			this.addNode(n);
-			this.addEdge(n,bottom);			
+			this.addEdge(n, bottom);
 		}
-		//implement algorithm as in paper
+		// implement algorithm as in paper
 		List<Object> agenda = new ArrayList<Object>();
-		for (Object obj :	this.backwardReachableNodes(abottom)){
-			agenda.add(obj); //access the bottom through its weight?
+		for (Object obj : this.backwardReachableNodes(abottom)) {
+			agenda.add(obj); // access the bottom through its weight?
 		}
-		//System.out.println(agenda.get(0));
+		// System.out.println(agenda.get(0));
 		int count = 0;
-		while (agenda.size()>0){
+		while (agenda.size() > 0) {
 			TTRLatticeNode ttrLat = (TTRLatticeNode) agenda.get(0);
-			//Node ntop = node(agenda.get(0));
+			// Node ntop = node(agenda.get(0));
 			agenda.remove(0);
-			//pop top one from agenda //bit different to the paper as this is much more like concept lattice stuff
-			//System.
-			
+			// pop top one from agenda //bit different to the paper as this is much more like concept lattice stuff
+			// System.
+
 			List<Object> subagenda = new ArrayList<Object>();
-			for (Object obj : agenda){
-				
-				//create minimal common supertype/join element (could be empty, in which case it's top).
-				TTRRecordType ttr = ((TTRLatticeNode)obj).ttr.minimumCommonSuperTypeBasic(ttrLat.ttr, new HashMap<Variable,Variable>());
-				TTRLatticeNode nweight = new TTRLatticeNode(ttr,new HashSet<TTRAustinianProp>()); //need to start anew!
+			for (Object obj : agenda) {
+
+				// create minimal common supertype/join element (could be empty, in which case it's top).
+				TTRRecordType ttr = ((TTRLatticeNode) obj).ttr.minimumCommonSuperTypeBasic(ttrLat.ttr,
+						new HashMap<Variable, Variable>());
+				TTRLatticeNode nweight = new TTRLatticeNode(ttr, new HashSet<TTRAustinianProp>()); // need to start
+																									// anew!
 				nweight.props.addAll(ttrLat.getProps());
-				nweight.props.addAll(((TTRLatticeNode)obj).getProps());
+				nweight.props.addAll(((TTRLatticeNode) obj).getProps());
 
 				System.out.println("NWEIGHT!");
-				 
-			
-				//order ttrLat appropriately to the supertype created here (and the others in the graph)??
-				if (containsTTRRecordType(nweight.ttr)){ // already in here, just order (and inherit, should have happened above?)
-					nweight.props.addAll(((TTRLatticeNode)this.nodeFromTTRRecordType(nweight.ttr).getWeight()).getProps());
+
+				// order ttrLat appropriately to the supertype created here (and the others in the graph)??
+				if (containsTTRRecordType(nweight.ttr)) { // already in here, just order (and inherit, should have
+															// happened above?)
+					nweight.props.addAll(((TTRLatticeNode) this.nodeFromTTRRecordType(nweight.ttr).getWeight())
+							.getProps());
 					System.out.println("contains!");
 					System.out.println(this.nodeFromTTRRecordType(nweight.ttr));
 					this.nodeFromTTRRecordType(nweight.ttr).setWeight(nweight);
-					//need to search to order.. i.e. go up as high as possible
-					order(this.nodeFromTTRRecordType(nweight.ttr),node(obj));
-					
-					if (this.reachableNodes(this.nodeFromTTRRecordType(nweight.ttr)).contains(node(obj))){
+					// need to search to order.. i.e. go up as high as possible
+					order(this.nodeFromTTRRecordType(nweight.ttr), node(obj));
+
+					if (this.reachableNodes(this.nodeFromTTRRecordType(nweight.ttr)).contains(node(obj))) {
 						continue;
 					}
-					
-					if (!node(obj).equals(this.nodeFromTTRRecordType(nweight.ttr))){
-						addEdge(this.nodeFromTTRRecordType(nweight.ttr),node(obj));
+
+					if (!node(obj).equals(this.nodeFromTTRRecordType(nweight.ttr))) {
+						addEdge(this.nodeFromTTRRecordType(nweight.ttr), node(obj));
 					}
-					
-					if (this.reachableNodes(this.nodeFromTTRRecordType(nweight.ttr)).contains(node(ttrLat))){
+
+					if (this.reachableNodes(this.nodeFromTTRRecordType(nweight.ttr)).contains(node(ttrLat))) {
 						continue;
 					}
-					
-					if (!node(ttrLat).equals(this.nodeFromTTRRecordType(nweight.ttr))){
-						addEdge(this.nodeFromTTRRecordType(nweight.ttr),node(ttrLat));
+
+					if (!node(ttrLat).equals(this.nodeFromTTRRecordType(nweight.ttr))) {
+						addEdge(this.nodeFromTTRRecordType(nweight.ttr), node(ttrLat));
 					}
 					continue;
-				} 
-				//otherwise new node, order it to its supertype
+				}
+				// otherwise new node, order it to its supertype
 				Node n = new Node(nweight);
 				addNode(n);
-				addEdge(n,node(obj));
-				addEdge(n,node(ttrLat));
-				//add it to the agenda for further supertyping
-				if (!agenda.contains(nweight)){
+				addEdge(n, node(obj));
+				addEdge(n, node(ttrLat));
+				// add it to the agenda for further supertyping
+				if (!agenda.contains(nweight)) {
 					subagenda.add(nweight);
 				}
 			}
-			//add all the new ones to the agenda for further subtype checking
+			// add all the new ones to the agenda for further subtype checking
 			agenda.addAll(subagenda);
 			System.out.println("agenda");
-			for (Object i : agenda){
+			for (Object i : agenda) {
 				System.out.println(i);
 			}
 			System.out.println("sss");
 			System.out.println(this.toString());
-			count+=1;
+			count += 1;
 			pause();
-			//if (count>0){break;}
+			// if (count>0){break;}
 		}
-		
+
 	}
-	
-	/*public void addNode(TTRRecordType ttr, Set probs){
-		
-	}*/
+
+	/*
+	 * public void addNode(TTRRecordType ttr, Set probs){
+	 * 
+	 * }
+	 */
 
 	/**
 	 * @param args
@@ -203,45 +206,38 @@ public class TTRLattice extends DirectedAcyclicGraph {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TTRLattice lattice = new TTRLattice();
-		double t = 1.0/3.0;
+		double t = 1.0 / 3.0;
 		TTRRecordType ttr = TTRRecordType.parse("[ x : e |p1==square(x) : t|p==purple(x) : t]");
 		TTRRecordType ttr2 = TTRRecordType.parse("[ x : e |p1==square(x) : t|p==yellow(x) : t]");
 		TTRRecordType ttr3 = TTRRecordType.parse("[ x : e |p1==circle(x) : t|p==yellow(x) : t]");
-		Set<TTRAustinianProp> s = new HashSet(Arrays.asList(new TTRAustinianProp(ttr,t,1)));
-		Set<TTRAustinianProp> s2 = new HashSet(Arrays.asList(new TTRAustinianProp(ttr2,t,2)));
-		Set<TTRAustinianProp> s3 = new HashSet(Arrays.asList(new TTRAustinianProp(ttr3,t,3)));
-		List<TTRRecordType> myttr =  Arrays.asList(ttr,ttr2,ttr3);
+		Set<TTRAustinianProp> s = new HashSet(Arrays.asList(new TTRAustinianProp(ttr, t, 1)));
+		Set<TTRAustinianProp> s2 = new HashSet(Arrays.asList(new TTRAustinianProp(ttr2, t, 2)));
+		Set<TTRAustinianProp> s3 = new HashSet(Arrays.asList(new TTRAustinianProp(ttr3, t, 3)));
+		List<TTRRecordType> myttr = Arrays.asList(ttr, ttr2, ttr3);
 		List<Set<TTRAustinianProp>> myprops = new ArrayList<Set<TTRAustinianProp>>();
 		myprops.add(s);
 		myprops.add(s2);
 		myprops.add(s3);
 		lattice.constructFromAtoms(myttr, myprops);
-		
+
 		/*
-		Node n = new Node(new TTRLatticeNode(ttr,s));
-		lattice.addNode(n);
-		TTRRecordType ttr1 = new TTRRecordType();
-		TTRLatticeNode tn1 = new TTRLatticeNode(ttr1,s);
-		Node n1 = new Node(tn1);
-		lattice.addNode(n1);
-		lattice.addEdge(n, n1);
-		*/
-		//lattice.
-		//lattice.addEdge(n1,lattice.bottom());
-		//System.out.println(lattice.downSet(tn1));
-		for (Object i : lattice.weightArray(lattice.nodes())){
+		 * Node n = new Node(new TTRLatticeNode(ttr,s)); lattice.addNode(n); TTRRecordType ttr1 = new TTRRecordType();
+		 * TTRLatticeNode tn1 = new TTRLatticeNode(ttr1,s); Node n1 = new Node(tn1); lattice.addNode(n1);
+		 * lattice.addEdge(n, n1);
+		 */
+		// lattice.
+		// lattice.addEdge(n1,lattice.bottom());
+		// System.out.println(lattice.downSet(tn1));
+		for (Object i : lattice.weightArray(lattice.nodes())) {
 			System.out.println(i.toString());
-			//System.out.println(((TTRLatticeNode) i));
-			//System.out.println(lattice.downSet((TTRLatticeNode)i));
-			//System.out.println(((TTRLatticeNode)i).ttr);
+			// System.out.println(((TTRLatticeNode) i));
+			// System.out.println(lattice.downSet((TTRLatticeNode)i));
+			// System.out.println(((TTRLatticeNode)i).ttr);
 		}
 		System.out.println("edges");
-		for (Object e : lattice.edges()){
+		for (Object e : lattice.edges()) {
 			System.out.println(e.toString());
 		}
-		
-		
-		
 
 	}
 
