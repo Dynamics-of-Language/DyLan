2d90a3e8192a775bd6e5685061cdf2eb75746ac1
Arash Ashrafzadeh <arashmath16@gmail.com>
2022-12-05 16:30:26 +0000

    Improves testComputeRInc implementation and uses a new resource as the grammarPath.

--- src/java/qmul/ds/learn/GeneratorTester.java
+++ src/java/qmul/ds/learn/GeneratorTester.java
@@ -13,24 +13,24 @@ import java.util.HashMap;
 import java.util.regex.Matcher;
 
 import qmul.ds.formula.TTRRecordType;
-//import qmul.ds.learn.GeneratorLearner;
 
 public class GeneratorTester
 {
-    //TODO make the appropriate changes here so that I don't have to have the same path here and in the GeneratorLearner class.
-    static final String corpusPath = "dsttr/corpus/CHILDES/eveTrainPairs/CHILDESconversion400Final.txt";//.replaceAll("/", Matcher.quoteReplacement(File.separator));
-    static final String grammarPath = "dsttr/resource/2013-ttr-learner-output/";//.replaceAll("/", Matcher.quoteReplacement(File.separator));
-    static GeneratorLearner g = new GeneratorLearner(grammarPath, corpusPath); // is this ok that I'm instansiating here?
+    // TODO make the appropriate changes here so that I don't have to have the same path here and in the GeneratorLearner class.
+    static final String corpusPath = "dsttr/corpus/CHILDES/eveTrainPairs/CHILDESconversion400Final.txt".replaceAll("/", Matcher.quoteReplacement(File.separator));
+    static final String grammarPath = "dsttr/resource/2022-learner2013-output/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+    static GeneratorLearner g = new GeneratorLearner(grammarPath, corpusPath); // TODO is this meaningful?
 
 
     public static void testComputeRInc()
     {
         // below is the semantics for "I took it" from "CHILDESconversion400Final", 8th sentence.
         TTRRecordType rG = TTRRecordType.parse("[x1==it : e|x==i : e|e1==take : es|p2==subj(e1, x) : t|p3==obj(e1, x1) : t|head==e1 : es]");
-        TTRRecordType rCurr = TTRRecordType.parse("[x==i : e|head==e1 : es]"); // is this a valid TTR for "I" only?
+        TTRRecordType rCurr = TTRRecordType.parse("[x==i : e|head==e1 : es]"); // TODO is this a valid RT for "I" only?
 
-//        System.out.println(rG.toString());
         TTRRecordType rInc = g.computeRInc(rG, rCurr);
+        System.out.println("rG = " + rG.toString());
+        System.out.println("rCurr = " + rCurr.toString());
         System.out.println("rInc = " + rInc.toString());
     }
 
