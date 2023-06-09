ef8883f1afefc8a1890cccf43b877e4bd2fac6d7
Arash Ashrafzadeh <arashmath16@gmail.com>
2023-06-09 15:37:09 +0100

    Adds generation evaluation classes.

---
+++ src/java/qmul/ds/learn/GeneratorEvaluator.java
@@ -0,0 +1,50 @@
+package qmul.ds.learn;
+
+import qmul.ds.InteractiveProbabilisticGenerator;
+
+import java.io.*;
+import java.util.List;
+import java.util.regex.Matcher;
+
+public class GeneratorEvaluator {
+
+    static final String corpusFolderPath = "dsttr/corpus/CHILDES/eveTrainPairs/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+    static public String corpusPath = corpusFolderPath + "amat.txt";//"AA-train-lower-396-unique-top1.txt";//"AAtrain-3.txt";//AA-full-lower-4000-Copy.txt";//""LC-CHILDESconversion400FinalCopy.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt"; //"AA-train-lower-396-matching-top1.txt";//"LC-CHILDESconversion800TestFinalCopy.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt";//"AAtrain-72.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt"; //"AAtrain-72.txt"; // "LC-CHILDESconversion396FinalCopy.txt"; // "LC-CHILDESconversion3200TrainFinal.txt"; //"LC-CHILDESconversion396FinalCopy.txt"; //"LC-CHILDESconversion400FinalCopy.txt";//"AAtrain-3.txt";//"CHILDESconversion100TestFinalCopy.txt";
+    static final String grammarPath = "dsttr/resource/2022-learner2013-output/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+
+    public void evaluateOnPerturbations(){
+        // Load a generator.
+        InteractiveProbabilisticGenerator generator = new InteractiveProbabilisticGenerator(grammarPath, grammarPath);
+        try {
+            // Load data.
+            List<PerturbationSample> data = PerturbationSample.loadPerturbationData("perturbations.txt");
+            for(PerturbationSample sample : data){
+                // Start generation with goal rG, until index pI is reached.
+                generator.setGoal(sample.rG);
+                int currentIndex = 0;
+                while(currentIndex <= sample.pI){
+                    generator.generateNextWord();
+                    currentIndex++;
+                }
+                // if index of word is reached, update rG and continue generation.
+                generator.setGoal(sample.rP);
+                // if generation was possible, continue ?
+                if (generator.generate()){
+                    // GOOD.
+                }
+                // if generation was not possible, decide if this is breaking or if you should backtrack.
+                else{
+                    // backtrackAndGenerate();  // Backtracks until the last point where subsumption happens.
+                    // also check until when you should backtrack..
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load perturbation data.");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/java/qmul/ds/learn/PerturbationSample.java
@@ -0,0 +1,83 @@
+package qmul.ds.learn;
+
+import qmul.ds.formula.TTRRecordType;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+
+public class PerturbationSample {
+    public TTRRecordType rG = null;
+    public TTRRecordType rP = null;
+    public int pI = -1;
+    public boolean isForward = false;
+    public int distance = -1;
+    public String pos = null;
+
+    static final String grammarFolderPath = "dsttr/resource/2022-learner2013-output/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+
+    public PerturbationSample(TTRRecordType rG, TTRRecordType rP, int pI, boolean isForward, int distance, String pos){
+        this.rG = rG;
+        this.rP = rP;
+        this.pI = pI;
+        this.isForward = isForward;
+        this.distance = distance;
+        this.pos = pos;
+    }
+
+    /**
+     * Reminder for the format of the perturbation data file:
+     * - rG is the original goal sem
+     * - rP is the perturbed goal sem
+     * - pI is the index of the word after which the goal is perturbed [I believe this means the last generated word before a perturbation taking place.]
+     *
+     * @param fileName
+     * @return
+     * @throws IOException
+     */
+    public static List<PerturbationSample> loadPerturbationData(String fileName) throws IOException {
+        List<PerturbationSample> samples = new ArrayList<>();
+
+        FileReader reader = new FileReader(grammarFolderPath + fileName);
+        BufferedReader stream = new BufferedReader(reader);
+        String line;
+        while ((line = stream.readLine()) != null) {
+            TTRRecordType rG = TTRRecordType.parse(line);
+            line = stream.readLine();
+            TTRRecordType rP = TTRRecordType.parse(line);
+            line = stream.readLine();
+            int pI = Integer.parseInt(line);
+            line = stream.readLine();
+            boolean isForward = Boolean.parseBoolean(line.toLowerCase());
+            line = stream.readLine();
+            int distance = Integer.parseInt(line);
+            line = stream.readLine();
+            String pos = line;
+            samples.add(new PerturbationSample(rG, rP, pI, isForward, distance, pos));
+            String emptyLine = stream.readLine(); // Empty line between samples.
+        }
+        stream.close();
+        return samples;
+    }
+
+
+    @Override
+    public String toString() {
+        return  "rG= " + rG +
+                ",\nrP= " + rP +
+                ",\npI= " + pI +
+                ",\nisForward= " + isForward +
+                ",\ndistance= " + distance +
+                ",\npos= " + pos;
+    }
+
+
+    public static void main(String[] args) throws IOException {  // Just to test the above.
+        List<PerturbationSample> samples = loadPerturbationData("perturbationData.txt");
+        System.out.println(samples);
+    }
+}
