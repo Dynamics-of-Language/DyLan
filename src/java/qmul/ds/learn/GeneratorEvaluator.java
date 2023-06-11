d99f9c50c7c0371e26f966014c5ad5b4713a09b7
Arash Ashrafzadeh <arashmath16@gmail.com>
2023-06-11 14:04:43 +0100

    Adds minor changes for generator evaluator class.

--- src/java/qmul/ds/learn/GeneratorEvaluator.java
+++ src/java/qmul/ds/learn/GeneratorEvaluator.java
@@ -9,8 +9,8 @@ import java.util.regex.Matcher;
 
 public class GeneratorEvaluator {
 
-    static final String corpusFolderPath = "dsttr/corpus/CHILDES/eveTrainPairs/".replaceAll("/", Matcher.quoteReplacement(File.separator));
-    static public String corpusPath = corpusFolderPath + "amat.txt";//"AA-train-lower-396-unique-top1.txt";//"AAtrain-3.txt";//AA-full-lower-4000-Copy.txt";//""LC-CHILDESconversion400FinalCopy.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt"; //"AA-train-lower-396-matching-top1.txt";//"LC-CHILDESconversion800TestFinalCopy.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt";//"AAtrain-72.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt"; //"AAtrain-72.txt"; // "LC-CHILDESconversion396FinalCopy.txt"; // "LC-CHILDESconversion3200TrainFinal.txt"; //"LC-CHILDESconversion396FinalCopy.txt"; //"LC-CHILDESconversion400FinalCopy.txt";//"AAtrain-3.txt";//"CHILDESconversion100TestFinalCopy.txt";
+//    static final String corpusFolderPath = "dsttr/corpus/CHILDES/eveTrainPairs/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+//    static public String corpusPath = corpusFolderPath + "amat.txt";//"AA-train-lower-396-unique-top1.txt";//"AAtrain-3.txt";//AA-full-lower-4000-Copy.txt";//""LC-CHILDESconversion400FinalCopy.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt"; //"AA-train-lower-396-matching-top1.txt";//"LC-CHILDESconversion800TestFinalCopy.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt";//"AAtrain-72.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt"; //"AAtrain-72.txt"; // "LC-CHILDESconversion396FinalCopy.txt"; // "LC-CHILDESconversion3200TrainFinal.txt"; //"LC-CHILDESconversion396FinalCopy.txt"; //"LC-CHILDESconversion400FinalCopy.txt";//"AAtrain-3.txt";//"CHILDESconversion100TestFinalCopy.txt";
     static final String grammarPath = "dsttr/resource/2022-learner2013-output/".replaceAll("/", Matcher.quoteReplacement(File.separator));
     protected static Logger logger = Logger.getLogger(GeneratorEvaluator.class);
 
@@ -32,7 +32,7 @@ public class GeneratorEvaluator {
                 }
                 logger.info("reached index " + sample.pI);
                 // When the index of the last word is reached, change goal to rP and continue generation.
-                generator.setGoal(sample.rP);
+                generator.setGoal(sample.rG);
                 generator.generate();
             }
         } catch (IOException e) {
--- src/java/qmul/ds/learn/PerturbationSample.java
+++ src/java/qmul/ds/learn/PerturbationSample.java
@@ -67,7 +67,7 @@ public class PerturbationSample {
 
     @Override
     public String toString() {
-        return  "rG= " + rG +
+        return  "\nrG= " + rG +
                 ",\nrP= " + rP +
                 ",\npI= " + pI +
                 ",\nisForward= " + isForward +
