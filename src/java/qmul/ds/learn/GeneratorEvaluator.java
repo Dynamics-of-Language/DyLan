e49f97453b9d7b083b11d07076f3b03f17b18b5f
Arash Ashrafzadeh <arashmath16@gmail.com>
2023-06-11 13:30:27 +0100

    Fixes a minor bug in GeneratorEvaluator.

--- src/java/qmul/ds/learn/GeneratorEvaluator.java
+++ src/java/qmul/ds/learn/GeneratorEvaluator.java
@@ -1,5 +1,6 @@
 package qmul.ds.learn;
 
+import org.apache.log4j.Logger;
 import qmul.ds.InteractiveProbabilisticGenerator;
 
 import java.io.*;
@@ -11,22 +12,26 @@ public class GeneratorEvaluator {
     static final String corpusFolderPath = "dsttr/corpus/CHILDES/eveTrainPairs/".replaceAll("/", Matcher.quoteReplacement(File.separator));
     static public String corpusPath = corpusFolderPath + "amat.txt";//"AA-train-lower-396-unique-top1.txt";//"AAtrain-3.txt";//AA-full-lower-4000-Copy.txt";//""LC-CHILDESconversion400FinalCopy.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt"; //"AA-train-lower-396-matching-top1.txt";//"LC-CHILDESconversion800TestFinalCopy.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt";//"AAtrain-72.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt"; //"AAtrain-72.txt"; // "LC-CHILDESconversion396FinalCopy.txt"; // "LC-CHILDESconversion3200TrainFinal.txt"; //"LC-CHILDESconversion396FinalCopy.txt"; //"LC-CHILDESconversion400FinalCopy.txt";//"AAtrain-3.txt";//"CHILDESconversion100TestFinalCopy.txt";
     static final String grammarPath = "dsttr/resource/2022-learner2013-output/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+    protected static Logger logger = Logger.getLogger(GeneratorEvaluator.class);
 
     public void evaluateOnPerturbations(){
         // Load a generator.
         InteractiveProbabilisticGenerator generator = new InteractiveProbabilisticGenerator(grammarPath, grammarPath);
         try {
             // Load data.
-            List<PerturbationSample> data = PerturbationSample.loadPerturbationData("perturbations.txt");
+            List<PerturbationSample> data = PerturbationSample.loadPerturbationData("perturbationData.txt");
             for(PerturbationSample sample : data){
+                generator.init();
                 // Start generation with goal rG, until index pI is reached.
+                logger.info("processing perturbation sample: " + sample.toString());
                 generator.setGoal(sample.rG);
                 int currentIndex = 0;
                 while(currentIndex <= sample.pI){
                     generator.generateNextWord();
                     currentIndex++;
                 }
-                // if index of word is reached, update rG and continue generation.
+                logger.info("reached index " + sample.pI);
+                // When the index of the last word is reached, change goal to rP and continue generation.
                 generator.setGoal(sample.rP);
                 generator.generate();
             }
