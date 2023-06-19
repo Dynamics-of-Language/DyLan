9c93b2c1dbe83bbf6972e60baa12d59bc0922756
Arash Ashrafzadeh <arashmath16@gmail.com>
2023-06-19 14:07:06 +0100

    Updates PerturbationSample and GeneratorEvaluator to write results to file.

--- src/java/qmul/ds/learn/GeneratorEvaluator.java
+++ src/java/qmul/ds/learn/GeneratorEvaluator.java
@@ -1,62 +1,94 @@
 package qmul.ds.learn;
 
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.ling.Word;
 import org.apache.log4j.Logger;
 import qmul.ds.InteractiveProbabilisticGenerator;
+import qmul.ds.dag.UtteredWord;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 
 public class GeneratorEvaluator {
 
-//    static final String corpusFolderPath = "dsttr/corpus/CHILDES/eveTrainPairs/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+    //    static final String corpusFolderPath = "dsttr/corpus/CHILDES/eveTrainPairs/".replaceAll("/", Matcher.quoteReplacement(File.separator));
 //    static public String corpusPath = corpusFolderPath + "amat.txt";//"AA-train-lower-396-unique-top1.txt";//"AAtrain-3.txt";//AA-full-lower-4000-Copy.txt";//""LC-CHILDESconversion400FinalCopy.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt";//"AA-train-lower-396-matching-top1.txt";//"AAtrain-3.txt"; //"AA-train-lower-396-matching-top1.txt";//"LC-CHILDESconversion800TestFinalCopy.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt";//"AAtrain-72.txt"; //"LC-CHILDESconversion3200TrainFinalCopy.txt"; //"AAtrain-72.txt"; // "LC-CHILDESconversion396FinalCopy.txt"; // "LC-CHILDESconversion3200TrainFinal.txt"; //"LC-CHILDESconversion396FinalCopy.txt"; //"LC-CHILDESconversion400FinalCopy.txt";//"AAtrain-3.txt";//"CHILDESconversion100TestFinalCopy.txt";
-    static final String grammarPath = "resource/2022-learner2013-output/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+    static final String grammarPath = "dsttr/resource/2022-learner2013-output/".replaceAll("/", Matcher.quoteReplacement(File.separator));
     protected static Logger logger = Logger.getLogger(GeneratorEvaluator.class);
-    
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_RED = "\u001B[31m";
+
     InteractiveProbabilisticGenerator generator;
-    
-    public GeneratorEvaluator(String grammarPath, String modelPath)
-    {
-    	generator = new InteractiveProbabilisticGenerator(grammarPath, modelPath);
+
+    public GeneratorEvaluator(String grammarPath, String modelPath) {
+        generator = new InteractiveProbabilisticGenerator(grammarPath, modelPath);
     }
-    
-    public void evaluateOnPerturbations(String perturbationsFile){
+
+    public void evaluateOnPerturbations(String perturbationsFile) {
         // Load a generator.
         try {
             // Load data.
             List<PerturbationSample> data = PerturbationSample.loadPerturbationData(perturbationsFile);
-            for(PerturbationSample sample : data){
-            	generator.setRepairGeneration(true);
+            List<PerturbationSample> fullyGeneratedPrtbs = new ArrayList<>();
+            List<PerturbationSample> partiallyGeneratedPrtbs = new ArrayList<>();
+            List<String> fullyGeneratedSentences = new ArrayList<>();
+            List<String> partiallyGeneratedSentences = new ArrayList<>();
+
+            for (PerturbationSample sample : data) {
+                generator.setRepairGeneration(true);
                 generator.init();
                 // Start generation with goal rG, until index pI is reached.
-                logger.info("processing perturbation sample: " + sample.toString());
+                logger.debug(ANSI_CYAN + "Processing perturbation sample: " + sample.toString() + ANSI_RESET);
                 generator.setGoal(sample.rG);
                 int currentIndex = 0;
-                while(currentIndex <= sample.pI){
+                while (currentIndex <= sample.pI) {
                     generator.generateNextWord();
                     currentIndex++;
                 }
-                logger.info("Perturbing goal to: "+sample.rP);
-                
+                logger.debug(ANSI_PURPLE + "Perturbing goal to: " + sample.rP + ANSI_RESET);
                 // When the index of the last word is reached, change goal to rP and continue generation.
                 generator.setGoal(sample.rP);
-                generator.generate();
+
+                Sentence<Word> generatedSentence = new Sentence<>();
+
+
+                if (generator.generate()) {
+                    logger.debug(ANSI_CYAN + "Fully generated: " + sample + ANSI_RESET);
+                    List<UtteredWord> sent = generator.getGenerated().getWords();
+                    sent.forEach((w) -> generatedSentence.add(new Word(w.word())));
+                    fullyGeneratedSentences.add(generatedSentence.toString());
+                    fullyGeneratedPrtbs.add(sample);
+                } else {
+                    logger.debug(ANSI_RED + "Partially generated: " + sample + ANSI_RESET);
+                    List<UtteredWord> sent = generator.getGenerated().getWords();
+                    sent.forEach((w) -> generatedSentence.add(new Word(w.word())));
+                    partiallyGeneratedPrtbs.add(sample);
+                    partiallyGeneratedSentences.add(generatedSentence.toString());
+                }
             }
+            PerturbationSample.writeEvalOutputTofFile(fullyGeneratedPrtbs, "prtbFullyGenerated.txt", fullyGeneratedSentences);
+            PerturbationSample.writeEvalOutputTofFile(partiallyGeneratedPrtbs, "prtbPartiallyGenerated.txt", partiallyGeneratedSentences);
+            logger.info(ANSI_GREEN + "Count of fully generated perturbations: " + fullyGeneratedPrtbs.size() + ANSI_RESET);
+            logger.info(ANSI_GREEN + "Count of partially generated perturbations: " + partiallyGeneratedPrtbs.size() + ANSI_RESET);
         } catch (IOException e) {
             throw new RuntimeException("Could not load perturbation data.");
         }
     }
 
-    public void setRepairGeneration(boolean b)
-    {
-    	generator.setRepairGeneration(b);
+    public void setRepairGeneration(boolean b) {
+        generator.setRepairGeneration(b);
     }
 
     public static void main(String[] args) {
         GeneratorEvaluator evaluator = new GeneratorEvaluator(grammarPath, grammarPath);
-        
-        
-        evaluator.evaluateOnPerturbations("perturbationData.txt");
+        evaluator.evaluateOnPerturbations("prtbAll.txt");
     }
 }
--- src/java/qmul/ds/learn/PerturbationSample.java
+++ src/java/qmul/ds/learn/PerturbationSample.java
@@ -1,29 +1,35 @@
 package qmul.ds.learn;
 
-import qmul.ds.Utterance;
+import org.apache.log4j.Logger;
+import qmul.ds.InteractiveProbabilisticGenerator;
 import qmul.ds.formula.TTRRecordType;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 
 public class PerturbationSample {
-    public Utterance originalSent = null;
+    public String originalSent = null;
     public TTRRecordType rG = null;
     public TTRRecordType rP = null;
-    public Utterance perturbedSent = null;
+    public String perturbedSent = null;
     public int pI = -1;
     public boolean isForward = false;
     public int distance = -1;
     public String pos = null;
 
-    static final String grammarFolderPath = GeneratorEvaluator.grammarPath;
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
 
-    public PerturbationSample(Utterance originalSent, TTRRecordType rG, TTRRecordType rP, Utterance perturbedSent, int pI, boolean isForward, int distance, String pos){
+
+    static final String grammarFolderPath = GeneratorEvaluator.grammarPath;
+    static final String grammarPath = "dsttr/resource/2022-learner2013-output/".replaceAll("/", Matcher.quoteReplacement(File.separator));
+    protected static org.apache.log4j.Logger logger = Logger.getLogger(PerturbationSample.class);
+
+    // --------------------- constructors ---------------------
+    public PerturbationSample(String originalSent, TTRRecordType rG, TTRRecordType rP, String perturbedSent, int pI, boolean isForward, int distance, String pos) {
         this.originalSent = originalSent;
         this.rG = rG;
         this.rP = rP;
@@ -34,14 +40,16 @@ public class PerturbationSample {
         this.pos = pos; // Currently not used.
     }
 
+    // --------------------- methods ---------------------
+
     /**
      * Reminder for the format of the perturbation data file:
      * - rG is the original goal sem
      * - rP is the perturbed goal sem
      * - pI is the index of the word after which the goal is perturbed [I believe this means the last generated word before a perturbation taking place.]
      *
-     * @param fileName
-     * @return
+     * @param fileName the name of the file containing the perturbation data
+     * @return a list of perturbation samples
      * @throws IOException
      */
     public static List<PerturbationSample> loadPerturbationData(String fileName) throws IOException {
@@ -51,13 +59,13 @@ public class PerturbationSample {
         BufferedReader stream = new BufferedReader(reader);
         String line;
         while ((line = stream.readLine()) != null) {
-            Utterance originalSent = new Utterance(line);
+            String originalSent = line;
             line = stream.readLine();
             TTRRecordType rG = TTRRecordType.parse(line);
             line = stream.readLine();
             TTRRecordType rP = TTRRecordType.parse(line);
             line = stream.readLine();
-            Utterance perturbedSent = new Utterance(line);
+            String perturbedSent = line;
             line = stream.readLine();
             int pI = Integer.parseInt(line);
             line = stream.readLine();
@@ -74,6 +82,82 @@ public class PerturbationSample {
     }
 
 
+    public static List<PerturbationSample> findGeneratableSamples(List<PerturbationSample> allSamples) {
+        InteractiveProbabilisticGenerator generator = new InteractiveProbabilisticGenerator(grammarPath, grammarPath);
+        logger.info("Generator loaded.");
+        logger.info("Number of all samples: " + allSamples.size());
+        List<PerturbationSample> generatableSamples = new ArrayList<>();
+
+        for (PerturbationSample sample : allSamples) {
+            generator.init();
+            generator.setGoal(sample.rP);
+            if (generator.generate()) {
+                generatableSamples.add(sample);
+                logger.trace("Able to generate sample: " + sample + "\n");
+            }
+        }
+        logger.info("Number of generatable samples: " + generatableSamples.size());
+        return generatableSamples;
+    }
+
+
+    public static void filterPerturbationData(String fileName) throws IOException {
+        // Criteria:
+        // - local, non-local
+        // forward, backward
+        // - pos
+
+        List<PerturbationSample> samples = loadPerturbationData(fileName);
+        List<PerturbationSample> generatableSamples = findGeneratableSamples(samples);
+        logger.info(ANSI_GREEN + "Number of generatable samples: " + generatableSamples.size() + ANSI_RESET);
+    }
+
+
+    public static void writePerturbationDataTofFile(List<PerturbationSample> samples, String fileName) {
+        logger.info("Number of all samples: " + samples.size());
+        String fileDir = grammarFolderPath + fileName;
+        PrintStream out = null;
+        FileOutputStream fileOpen = null;
+        try {
+            fileOpen = new FileOutputStream(fileDir);
+            out = new PrintStream(fileOpen);
+            for (PerturbationSample sample : samples) {
+                out.print(sample.toStringAsRows());
+                out.print("\n");
+            }
+        } catch (Exception e) {
+            logger.error(ANSI_RED + "Couldn't write to \"" + fileDir + "\"!" + ANSI_RESET);
+        } finally {
+            if (out != null)
+                out.close();
+        }
+    }
+
+
+    public static void writeEvalOutputTofFile(List<PerturbationSample> samples, String fileName, List<String> generated) {
+        logger.info("Number of samples in" + fileName + ": " + samples.size());
+        String fileDir = grammarFolderPath + fileName;
+        PrintStream out = null;
+        FileOutputStream fileOpen = null;
+        try {
+            fileOpen = new FileOutputStream(fileDir);
+            out = new PrintStream(fileOpen);
+            for (int i=0; i<samples.size(); i++) {
+                PerturbationSample sample = samples.get(i);
+                String generatedSentence = generated.get(i);
+                out.print(sample.toStringAsRows());
+                out.print(generatedSentence+"\n");
+                out.print("\n");
+            }
+        } catch (Exception e) {
+            logger.error(ANSI_RED + "Couldn't write to \"" + fileDir + "\"!" + ANSI_RESET);
+        } finally {
+            if (out != null)
+                out.close();
+        }
+    }
+
+
     @Override
     public String toString() {
         return  "\noriginalSent= " + originalSent +
@@ -87,9 +171,21 @@ public class PerturbationSample {
     }
 
 
+    public String toStringAsRows() {
+        return  originalSent +
+                "\n" + rG +
+                "\n" + rP +
+                "\n" + perturbedSent +
+                "\n" + pI +
+                "\n" + isForward +
+                "\n" + distance +
+                "\n" + pos + "\n";
+    }
+
+
     public static void main(String[] args) throws IOException {  // Just to test the above.
-        List<PerturbationSample> samples = loadPerturbationData("perturbations.txt");
-        for (PerturbationSample sample : samples)
-            System.out.println(sample);
+        List<PerturbationSample> samples = loadPerturbationData("prtbAll.txt");
+        List<PerturbationSample> generatableSamples = findGeneratableSamples(samples);
+        writePerturbationDataTofFile(generatableSamples, "prtbGeneratables.txt");
     }
 }
